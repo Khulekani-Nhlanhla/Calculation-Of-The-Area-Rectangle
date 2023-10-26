716cc50b5e9be168d769a060bdacae31ff767c56
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-26 08:50:08 +0200

    Initial commit

---
+++ src/AreaOfRectangle.java
@@ -0,0 +1,20 @@
+public class AreaOfRectangle {
+    public int sideHeight;
+    public int sideBase;
+    public int areaOfRectangle;
+
+    public void getData(int b,int h){
+        int sideHeight = h;
+        int sideBase = b;
+
+    }
+
+    public int computeField(){
+        public int result;
+        AreaOfRectangle = ((sideHeight*sideBase)/2);
+        return AreaOfRectangle;
+    }
+    public int fieldDisplay(){
+        System.out.println("The area of this triangle is: ");
+    }
+}
---
+++ src/PrimaryClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PrimaryClass {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("This program calculates the area of a triangle.(base * height)/2");
+        System.out.println("Please enter the size of base height");
+        getData()
+    }
+}
